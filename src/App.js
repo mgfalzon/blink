c8621298d3f9a36e89bc6f77a80d23a27a12b12a
ajbab99 <66225791+ajbab99@users.noreply.github.com>
2020-09-01 20:27:55 -0400

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserView, MobileView} from 'react-device-detect'
+
 
 import './Style.css'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
+import Footer from './Components/Footer'
 
 const App = () => (
-    <Router>
-      <Navbar />
-      <Switch>
-         <Route path='/' exact component={Home}/>
-      </Switch>
-    </Router>
+   <>
+    <BrowserView>
+        <Router>
+            <Navbar />
+            <Switch>
+              <Route path='/' exact component={Home}/>
+            </Switch>
+         </Router>
+      <Footer />
+    </BrowserView>
+    </>
 )
+
+
 
 export default App;
